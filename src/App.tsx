8d5746ba6401e35ec2d78f8cd74f69f3e7c247c9
Hurado <piaszczynskiarkadiusz@gmail.com>
2022-08-19 19:04:40 +0200

    Changes for data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+
+
 import React, { Component } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import KIDS from './pages/KIDS'
@@ -5,16 +7,112 @@ import WOMEN from './pages/WOMEN'
 import MEN from './pages/MEN'
 import NavBar from './components/Navbar'
 import Home from './pages/Home'
+import { useQuery ,gql} from '@apollo/client'
 
+const GET_CATEGORIES = gql`
+  query GetCategories {
+	categories {
+	  name
+	}
+  }  
+`;
+
+
+interface CategoriesProps {
+	categories: string,
+	name: string
+}
 
 
 export default class App extends Component {
+	constructor(props:CategoriesProps) {
+		super(props);
+		this.state = {
+			categories: String,
+			name: String,
+		};
+
+		this.create = this.create.bind(this);
+		this.update = this.update.bind(this);
+		this.delete = this.delete.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+		
+	
+		create(e:any) {
+			// add entity - POST
+			e.preventDefault();
+
+			// creates entity
+			fetch("http://localhost:4000", {
+				"method": "POST",
+				"headers": {
+				  "content-type": "application/json",
+				  "accept": "application/json"
+				},
+				"body": JSON.stringify({
+				  name: this.state.name,
+				  notes: this.state.notes
+				})
+			  })
+			  .then(response => response.json())
+			  .then(response => {
+				console.log(response)
+			  })
+			  .catch(err => {
+				console.log(err);
+			  });
+			}
+			update(e: { preventDefault: () => void }, id:any, name:any) {
+				// update entity - PUT
+				e.preventDefault();
+				// this will update entries with PUT
+				fetch("http://localhost:4000", {
+					"method": "PUT",
+					"headers": {
+						"content-type": "application/json",
+						"accept": "application/json"
+					},
+					"body": JSON.stringify({
+						_id: this.state.id,
+						name: this.state.name,
+						// notes: this.state.notes
+					})
+					})
+					.then(response => response.json())
+					.then(response => { console.log(response);
+					})
+					.catch(err =>{ console.log(err); });
+				}
+				delete(e:any) {
+					// delete entity - DELETE
+					e.preventDefault();
+					// deletes entities
+					fetch(`http://localhost:4000/categories/name/_id/$%7Bthis.state.id%7D/${this.state.id}`, {
+					  "method": "DELETE",
+					  "headers": {
+					
+					  }
+					})
+					.then(response => response.json())
+					.then(response => {
+					  console.log(response);
+					})
+					.catch(err => {
+					  console.log(err);
+					});
+				  }
+				  handleChange(changeObject:any) {
+					this.setState(changeObject)
+				  }
+
 	render() {
+		const newLocal = data.categories[0].name
 		return (
 			<>
 				<NavBar />
 				<Routes>
-					<Route path='/' element={<Home/>} />
+					<Route path='/' element={newLocal} />
 					<Route path='/WOMEN' element={<WOMEN />} />
 					<Route path='/MEN' element={<MEN />} />
 					<Route path='/KIDS' element={<KIDS />} />
--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,77 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	gql,from, HttpLink
+  } from "@apollo/client";
+import { onError } from '@apollo/client/link/error'
 
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000',
+})
 
+// Log any GraphQL errors or network error that occurred
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.error(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+    )
+  if (networkError) console.error(`[Network error]: ${JSON.stringify(networkError, null, 2)})`)
+})
 
+const client = new ApolloClient({
+	link: from([errorLink, httpLink]), // `httpLink` must be the last
+	cache: new InMemoryCache(),
+  });
+ 
+
+ client 
+  .query({
+	query: gql`
+	query {categories {
+		name 
+		products {
+		  id
+		  name
+		  inStock
+		  gallery
+		  description
+		  category
+		  attributes {
+			id
+			name
+			type
+			items {
+			  displayValue
+			  value
+			  id
+			}
+		  }
+		  prices {
+			currency {
+			  label
+			  symbol
+			}
+			amount
+		  }
+		  brand
+		}
+	  }	}
+	  `,
+})
+
+  .then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
+	<ApolloProvider client={client}>
 	<React.StrictMode>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
+	</ApolloProvider>
 )
